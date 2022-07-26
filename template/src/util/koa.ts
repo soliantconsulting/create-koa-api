b73ef18d832d3b44da858f2a7370e49efd967a61
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2022-07-26 17:19:19 +0200

    fix(template): use file protocol URLs for composite router

--- template/src/util/koa.ts
+++ template/src/util/koa.ts
@@ -1,5 +1,6 @@
 import {promises as fs} from 'fs';
 import path from 'path';
+import {pathToFileURL} from 'url';
 import Router from '@koa/router';
 
 export const compositeRouter = async (
@@ -11,7 +12,7 @@ export const compositeRouter = async (
 
     for (const file of files) {
         if ((file.endsWith('.js') || file.endsWith('.ts')) && !file.startsWith('index')) {
-            const module = await import(path.join(directory, file)) as {default ?: unknown};
+            const module = await import(pathToFileURL(path.join(directory, file)).toString()) as {default ?: unknown};
 
             if (module.default instanceof Router) {
                 router.use(module.default.routes());
