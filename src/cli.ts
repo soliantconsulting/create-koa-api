2e59795d04f78ed7ad8883ee9abbc791d6f01d4c
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2024-01-19 15:35:45 +0100

    style: run biome format

--- src/cli.ts
+++ src/cli.ts
@@ -257,9 +257,12 @@ const tasks = new Listr<Context>(
 try {
     await tasks.run();
 
-    logger.log(ListrLogLevels.COMPLETED, 'Project creation successful, you must run the pipeline manually once.');
-    logger.log(ListrLogLevels.COMPLETED, 'You can find additional code snippets here:');
-    logger.log(ListrLogLevels.COMPLETED, 'https://github.com/soliantconsulting/koa-api-recipes');
+    logger.log(
+        ListrLogLevels.COMPLETED,
+        "Project creation successful, you must run the pipeline manually once.",
+    );
+    logger.log(ListrLogLevels.COMPLETED, "You can find additional code snippets here:");
+    logger.log(ListrLogLevels.COMPLETED, "https://github.com/soliantconsulting/koa-api-recipes");
 } catch (error) {
     logger.log(ListrLogLevels.FAILED, error as string);
 }
